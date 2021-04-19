1192ffc8dac36446b0556820d05978dfbf81f2b5
Matheus Allein <mtsallein@gmail.com>
2021-04-19 09:32:21 -0300

    Updating program: adding recordings youtube and slide links

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -16,24 +16,22 @@ function Home() {
               align="center"
               color="primary"
             >
-              LIneA Minicurso
-            </Typography>
-            <br />
-            <Typography variant="h4" align="center" color="primary">
-              Introdução a ferramentas computacionais aplicadas à e-Astronomia
+              Minicurso
             </Typography>
             <Typography variant="h6" align="center" color="primary">
               (Projeto Piloto)
             </Typography>
             <br />
+            <Typography variant="h4" align="center" color="primary">
+              <strong>
+                Introdução a ferramentas computacionais aplicadas à e-Astronomia
+              </strong>
+            </Typography>
+            <br />
             <br />
             <Typography variant="h5" align="center" color="primary">
-              O LIneA abre as inscrições para o primeiro módulo do minicurso
-              &ldquo;
-              <strong>
-                Introdução a ferramentas computacionais aplicadas à Astronomia
-              </strong>
-              &rdquo; a nível de doutorado e pós-doutorado, totalmente gratuito.
+              O LIneA abre as inscrições para o primeiro módulo do minicurso a
+              nível de doutorado e pós-doutorado, totalmente gratuito.
             </Typography>
             <br />
             <Typography variant="h5" align="center" color="primary">
